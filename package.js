2f17a8131d51cb9ac0ce04a4ac51ac662d4fac8c
GreenTaeyn <112050678+GreenTeayn@users.noreply.github.com>
2025-07-10 15:50:06 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "HOMEY",
+  "version": "1.0.0",
+  "description": "Simple Aternos AFK Bot",
+  "main": "index.js",
+  "dependencies": {
+    "minecraft-protocol": "^1.34.2",
+    "express": "^4.18.2"
+  }
+}
